7162528a2e11eab47602f36b1b9651e62156af6b
iOgre <igorvadimovich@outlook.com>
2018-04-23 03:00:40 +0500

    changes

--- provider.js
+++ provider.js
@@ -1,33 +1,32 @@
 
 var provider = {
-  suggest : function(request, options) {
+  suggest :  function(request, options) {
     var resultArray = [];
     var suggest = new ymaps.suggest(request);
-    var result = suggest.then(items => {
-      console.log('items before '+ items.length);
-      var include = false;
-      
+    var result = suggest.then( items => {
+      console.log('items before ' + items.length);
+      var countryCode = '----';
       for (const i of items) {
-          var gc = new ymaps.geocode(i.value).then(res => {
+        var gc =  new ymaps.geocode(i.value).then(res => {
           var firstGeo = res.geoObjects.get(0);
-          var countryCode = firstGeo.getCountryCode();
-         // if(countryCode == 'RU')
+          countryCode = firstGeo.getCountryCode();
+          // if(countryCode == 'RU')
           {
-              resultArray.push(
-                {
-                  value: i.value,
-                displayName: countryCode + ' ' + i.displayName
-                });
+        
           }
         });
+        resultArray.push({
+          value: countryCode + ' ' + i.value,
+          displayName: countryCode + ' ' + i.displayName
+        });
+    
       }
       console.log('items iterated');
       console.log(resultArray);
       return ymaps.vow.resolve(resultArray);
-    
+  
     });
-    return suggest;
-    return ymaps.vow.resolve(result);
+        return ymaps.vow.resolve(result);
     
     
     
